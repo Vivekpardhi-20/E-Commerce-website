9bd9a698410841fcafe6f2f84f9149ffdca542c1
Vivek <pardhivivek82@gmail.com>
2025-08-26 16:09:27 +0530

    ...

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,9 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, isDevMode } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './app/auth.interceptor';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(App, {
   providers: [
@@ -14,6 +15,9 @@ bootstrapApplication(App, {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
       multi: true
-    }
+    }, provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 }).catch(err => console.error(err));
